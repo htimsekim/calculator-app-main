dad4efa010a68b4aecbea26980fcf322759ace18
mike <mls.mikesmith@gmail.com>
2022-03-01 14:49:59 -0600

    finishing touches, need to test operations

--- js/main.js
+++ js/main.js
@@ -19,3 +19,133 @@ function changeTheme() {
     }
 
 }
+
+let firstNumber = 0;
+let secondNumber = 0;
+let numberSplitter;
+let total;
+let displayTotal = "new";
+let functionKeys = ["DEL", "+", "-", ".", "/", "x", "RESET", "="]
+let functionPressed = false; /* prevent multiple operators */
+
+let lastPressedFunction;
+
+document.querySelectorAll(".buttonNumber").forEach(item => {
+    item.addEventListener("click", event => {            
+        if (lastPressedFunction === "=") {
+            resetScreen();
+            lastPressedFunction = "";
+            functionPressed = true;
+        }
+        if (displayTotal === "new") {
+            if (item.innerHTML !== "0") {
+                displayTotal = item.innerHTML;
+                updateScreen()
+                functionPressed = false;
+            }
+        } else {
+            displayTotal += item.innerHTML;
+            updateScreen();
+        }
+    }
+    )
+    
+})
+
+document.querySelector(".buttonReset").addEventListener("click", event => {
+    resetScreen();
+})
+
+
+document.querySelector(".buttonDel").addEventListener("click", event => {
+    if (displayTotal !== "new") {
+        displayTotal = displayTotal.slice(0,-1);
+        updateScreen()
+        if (displayTotal === "") {
+            resetScreen();
+        }
+    }
+})
+
+function addSymbolToScreen(operation) {
+    if (displayTotal !== "new" && functionPressed !== true) {
+        firstNumber = parseFloat(displayTotal);
+        flashScreen();
+        functionPressed = true;
+        lastPressedFunction = operation;
+        displayTotal += operation;
+        updateScreen();
+    }    
+}
+
+document.querySelector(".buttonPlus").addEventListener("click", event => {
+    addSymbolToScreen("+");
+})
+
+document.querySelector(".buttonMinus").addEventListener("click", event => {
+    addSymbolToScreen("-");
+})
+
+document.querySelector(".buttonMult").addEventListener("click", event => {
+    addSymbolToScreen("x");
+})
+
+document.querySelector(".buttonDiv").addEventListener("click", event => {
+    addSymbolToScreen("/");
+})
+
+document.querySelector(".buttonEqual").addEventListener("click", event => {
+    numberSplitter = displayTotal.split(lastPressedFunction);
+    secondNumber = parseFloat(numberSplitter[1]);
+    switch (lastPressedFunction) {
+        case "":
+            updateScreen();
+            lastPressedFunction = "=";
+            break;
+        case "+":
+            displayTotal = String(firstNumber + secondNumber);
+            updateScreen();
+            lastPressedFunction = "=";
+            break;
+        case "-":
+            displayTotal = String(firstNumber - secondNumber);
+            updateScreen();
+            lastPressedFunction = "=";
+            break;
+        case "x":
+            // idk why but i was getting weird stuff like 2.1 * 3 = 6.30000000000000001 so get rounded i guess
+            displayTotal = String(Math.round(firstNumber * secondNumber * 1000) / 1000);
+            updateScreen();
+            lastPressedFunction = "=";
+            break;
+        case "/":
+            displayTotal = String(Math.round(1000 * (firstNumber / secondNumber)) / 1000);
+            updateScreen();
+            lastPressedFunction = "=";
+            break;
+    }
+})
+
+function updateScreen() {
+    document.querySelector(".calcScreen").innerHTML = addCommas(displayTotal);
+    flashScreen();
+}
+
+function resetScreen() {
+    document.querySelector(".calcScreen").innerHTML = "0";
+    displayTotal = "new";
+    functionPressed = false;
+    lastPressedFunction = "";
+    flashScreen();
+}
+
+function addCommas(numb) {
+    var str = numb.toString().split(".");
+    str[0] = str[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return str.join(".");
+}
+
+function flashScreen() {
+    document.querySelector(".calcScreen").style.color = "var(--screen-background)";
+    setTimeout(() => {document.querySelector(".calcScreen").style.color = "var(--screen-text)"}, 100);
+}
